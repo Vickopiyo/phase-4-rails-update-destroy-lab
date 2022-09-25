41ca6fa33fc333291526955d23107731fc616606
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-25 23:10:58 +0300

    commit tree 0e98ce452dd240319f866f64e0723c3628a25619

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound , with: :error_response
   # GET /plants
   def index
     plants = Plant.all
@@ -8,8 +8,15 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
+  end
+  # UPDATE 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+
   end
 
   # POST /plants
@@ -17,10 +24,25 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  # DELETE plant
+  def destroy
+    plant = find_plant
+    plant.destroy
+  end
 
   private
+  def find_plant
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+    
+  def error_response
+    render json: { error: "Plant not found"}, status: :not_found
+    end
+
+
+
 end
